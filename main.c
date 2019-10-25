583d4da4995b9c9bad4bc07ba2e5603747bf4aa5
elliott <elliott@localhost>
2019-10-25 14:22:45 -0700

    LCD Module performing "ok".  Message shifts to the left.  Handles 40 character strings.

--- main.c
+++ main.c
@@ -15,15 +15,17 @@
 #include <stdint.h>        /* For uint8_t definition */
 #include <stdbool.h>       /* For true/false definition */
 #include <stdlib.h>        /* For string manipulation   */
-#include <string.h>        /* For string manipulation   */
-//#include <cstring>         /* For string manipulation   */
+
 #endif
 
 #include "system.h"        /* System funct/params, like osc/peripheral config */
 #include "user.h"          /* User funct/params, such as InitApp */
-#include "lcd.h"           /* LCD module funct/params, like LCD_Module_Initialize */
-#include "ax_25.h"         /* AX.25 funct/params, like Emit audio */
-/******************************************************************************/
+#include "./Components/LCD Module/lcd_module.h"           /* LCD module funct/params, like LCD_Module_Initialize */
+#include "./Components/Data Link Layer/data_link.h"       /* Data link funct/params, like Emit audio */
+#include "./Components/GPS Serial Port/gps_serial_port.h" /* GPS and Serial Port funct/params */
+#include "./Components/Morse Code Interpreter/morse_code_interpreter.h" 
+#include "./Components/Transport Layer/transport.h"       /* Transport funct/params */
+
 /* User Global Variable Declaration                                           */
 /******************************************************************************/
 
@@ -44,27 +46,39 @@ void main(void) {
 
     /* Initialize I/O and Peripherals for application */
     InitApp();
-    //Emit_Audio_1200Hz_Square();
-    //initialize a message to display on the LCD module
-    unsigned char strmessage[16] = "Hello ELMAN, you rule.";
+    //  Emit_Audio_1200Hz_Square();
+    //  Initialize a message to display on the LCD module
+    char message[40] = "Hello World!!!";
     
+
+    //  Display the message 
+    int len = strlen(message);
     // Write to 1st line of DD RAM
-    for (int i = 0; i < 8; i++) {
-        Set_Instruction(WRITE_CG_OR_DD_RAM, strmessage[i]);
+    for(int i = 0; i < len; i++)
+    {
+        Set_Instruction(WRITE_CG_OR_DD_RAM, message[i]);
         Wait();
     }
-
     // Write to 2nd line of DD RAM
-    //Set_DD_RAM_Address(0x40);
-    Set_Instruction(WRITE_INSTRUCTION, 0xC0);
-    //Wait();
-   // __delay_ms(20);
+    Set_Instruction(WRITE_INSTRUCTION,192);// SET_DD_RAM_ADDRESS & SECOND_LINE_ADDRESS);
+    //Wait();  
+    // Set entry mode.
     Set_Instruction(WRITE_INSTRUCTION, ENTRY_MODE_SET);
-    Wait();
-
-    for (int j = 8; j < strlen(strmessage); j++) {
-        Set_Instruction(WRITE_CG_OR_DD_RAM, strmessage[j]);
+    //  Write all but the first 8 characters to the second half of the LCD module
+    for(int j = 8; j < len; j++) 
+    {
         Wait();
+        Set_Instruction(WRITE_CG_OR_DD_RAM,message[j]);
+    }
+    
+    
+    //  Now the LCD module contains the message in both of the two line display
+    //  Shift left to see the whole message.
+    for(int l = 0; l < len; l++)
+    {
+        Set_Instruction(WRITE_INSTRUCTION,SHIFT_LEFT);
+        Wait();
+        __delay_ms(4000);
     }
 
 }
